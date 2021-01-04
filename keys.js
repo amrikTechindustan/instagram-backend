9b99f706d5596d00835fe801f21d7d105a5ba8ff
amrikTechindustan <amrik@techindustan.com>
2021-01-04 15:34:14 +0530

    first_commit

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://AmrikThakur:as1fPV2nkDmNW8go@cluster0.e66kz.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET:"efefefrgrgn"
+}
---
+++ routes/post.js
@@ -0,0 +1,45 @@
+const express=require('express')
+const router=express.Router()
+const mongoose=require('mongoose')
+const Post=require('../models/post  ')
+const requireLogin=require('../middleware/requireLogin')
+
+
+router.get('/allpost',(req,res)=>{
+    Post.find()
+    .populate("postedBy","_id")
+    .then(posts=>{
+        res.json({posts})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+router.post('/createpost',requireLogin,(req,res)=>{
+    const {title,body}=req.body
+    if(!title || !body){
+        return res.status(422).json({error:"please add all the fields"})
+    }
+   const post=new post({
+        title,
+        body,
+        postedBy:req.user
+   })
+   post.save().then(result=>{
+       res.json({post:result})
+   })
+   .catch(err=>{
+       console.log(err)
+   })
+})
+router.get('/mypost',(req,res)=>{
+    Post.find({postedBy:req.user._id})
+    .populate("PostedBy","_id name")
+    .then(mypost=>{
+        res.json(mypost)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+module.exports=router
